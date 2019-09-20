3b745808034634aaa0eaf2d470564284eb0dcad3
Hantute <ahinfohistopro@gmail.com>
2019-09-20 12:30:03 +0200

    mise a jour

--- application/views/produit/detail.php
+++ application/views/produit/detail.php
@@ -1,27 +1,52 @@
-
+<?php $limite=$detail->stock_produit;
+$cpt=0;
+$prix=$detail->prix_produit;
+$facture = $prix * $cpt;
+var_dump($prix);
+var_dump($facture);?>
 
 <table border='15' bgcolor=#d0ff00 text-aligne='center'>
 	<tr border="10" bgcolor='#a4c2f4' width='150'>
 	<th width='100'> Numéro de code du produit</th>
 	<th width='250'> Nom du produit</th>
-	<th width='100'> Stock du produit</th>
-	<th width='100'> Prix du produit</th>
-	<th width='500'> Description du produit</th>
-	<th width='250'> Catégorie du produit</th>
-	<th width='100'> Type de produit</th>
-	<th width='100'> classe du produit</th>
-	<th width='100'> Vaisseau correspondant au produit</th>
+	<th width='100'> Stock</th>
+	<th width='100'> Prix à l'unité</th>
+	<th width='500'> Description</th>
+	<th width='150'> Catégorie</th>
+	<th width='100'> Type du modèle</th>
+	<th width='100'> Classe du modèle</th>
+        <th width='100'> Quantité commandé</th>
+        <th width='100'> Prix total</th>
+	<th width='100'> Détail du Vaisseau</th>
 	</tr>
 	<tr border='10' bgcolor=#6ff7ae width='150' text-align='center'>
 	<td><?php echo $detail->id_produit;?></td>
 	<td><?php echo $detail->nom_produit;?></td>
 	<td><?php echo $detail->stock_produit;?></td>
-	<td><?php echo $detail->prix_produit;?>€</td>
+        <td><input disabled class='col-10' id="prix" value="<?php echo $prix ; ?>">€</td>
 	<td><?php echo $detail->Description;?></td>
 	<td><?php echo $detail->categorie_produit;?></td>
 	<td><?php echo $detail->nom_type;?></td>
 	<td><?php echo $detail->classe_vaisseau;?></td>
-	<td><a href="<?php echo site_url("/vaisseau/detail/").$detail->id_vaisseau;?>"><?php echo $detail->nom_vaisseau;?></td>
-
-
+        <td><select id='commande'> <?php for($cpt=0; $cpt<=$limite;$cpt++){ ?>
+                                            <option value='<?php echo $cpt ;?>'><?php echo $cpt ;?></option>
+                                         <?php } ?></select></td>
+        <td><input disabled type='float' class='col-10' id='facture' value='<?php echo $facture ;?>'>€</td>
+        <td><a href="<?php echo site_url("/vaisseau/detail/").$detail->id_vaisseau;?>"><?php echo $detail->nom_vaisseau;?></a></td>
+        
 </table>
+<script>
+    $("#commande").change(function()
+        {
+            console.log("bonjour");
+            
+            let $quantite=$("#commande").val();
+            let $prix=$("#prix").val();
+            console.log($quantite);
+            console.log($prix);
+            $facture= $prix*$quantite;
+            $("#facture").appendTo("#facture").val($facture);
+        
+    
+        });
+</script>
--- application/views/produit/liste.php
+++ application/views/produit/liste.php
@@ -26,42 +26,94 @@
 			<?php
 			foreach ($liste as $row)
 			{
-                            $limite=$row->stock_produit;?>
-			    <tr border='10' bgcolor='#6ff7ae' width='150' text-aligne='center'>
-				<td width='400' text-align='center'><?php echo $row->photo_vaisseau ;?>"</td>
-				<td width='75' text-align='center'><?php echo $row->id_produit ; ?>"</td>\n";
-				<td width='150' text-align='center'><?php echo"<a href=".site_url("produit/detail/").$row->id_produit.">".$row->nom_produit ;?></td>
-				<td width='100' text-align='center' id='prix' value='<?php echo $row->prix_produit ;?>'><?php echo $row->prix_produit ;?>€</td>
+                            $facture=0;
+                            $id=$row->id_produit;
+                            $prix=$row->prix_produit;
+                            $limite=$row->stock_produit;
+                            $qtot=0;
+                            $Prixtot=0;?>
+                        <tr border='10' bgcolor='#6ff7ae' width='150' id='<?php echo $id ;?>' class='click' value='<?php echo $id ;?>' text-align='center'>
+				<td width='400' text-align='center' ><?php echo $row->photo_vaisseau ;?></td>
+                                <td><input disabled width='75' class='col-8' text-align='center' id='id<?php echo $id ; ?>' value='<?php echo $id ; ?>' ></td>
+                                <td width='100' text-align='center'><?php echo"<a href=".site_url("produit/detail/").$id.">".$row->nom_produit ;?></a></td>
+                                <td><input disabled width='100' class='col-10' text-align='center' id='prix<?php echo $id ; ?>' value='<?php echo $row->prix_produit ;?>'> €</td>
 				<td width='100' text-align='center'><?php echo $row->categorie_produit ;?></td>
                                 <td width='100' text-align='center'><?php echo $limite ;?></td>
-                                <td width='100' text-align='center'><select id='commande'>
+                                <td width='100' text-align='center'><select id="quantite<?php echo $id ; ?>" class='commande'>
                                         <?php for($cpt=0; $cpt<=$limite;$cpt++){ ?>
                                             <option value='<?php echo $cpt ;?>'><?php echo $cpt ;?></option>
                                          <?php } ?>
                                         </select>        
                                 </td>
-                                <td width='100' text-align='center' id='facture' value='<?php echo $facture=0 ;?>'>€</td> 
+                                <td class='facturation' ><input disabled class='col-8' width='100' text-align='center' id='facture<?php echo $id ; ?>' value='<?php echo $facture ;?>'> €</td> 
 					<?php if ($this->session->user->id_autorisation == "1")
 						{ ?>
                                 <td width='100' text-align='center'><?php echo "<a href=".site_url("/produit/modification/").$row->id_produit.">";?>modification</a></td>
-				<td width='100' text-align='center'><?php echo "<a href=".site_url("/produit/suppression/").$row->id_produit.">" ;?>suppression</td>
+                                <td width='100' text-align='center'><?php echo "<a href=".site_url("/produit/suppression/").$row->id_produit.">" ;?>suppression</a></td>
 						<?php } ?>
-					</tr>
-			<?php }
-
+			</tr>
+			<?php 
+                        $Max=$row->id_produit;
+                        $MaxId = $Max+1;
+                        }                        
 			?>
-
+                        <tr border="10" bgcolor='#6ff7a' width='150' id='resultat' text-align='center'>
+                            <TH width='400' hidden='TRUE'></TH>
+                            <TH width="75" hidden='TRUE'></TH>
+                            <TH width='150' hidden='TRUE'></TH>
+                            <TH width='100' hidden='TRUE'></TH>
+                            <TH width='100' hidden='TRUE'> </TH>
+                        </tr>
         	</table>
-    <script>
-            $("#commande").change(function()
+            <div class='row'>
+                <div class="col-6"></div>
+                <div class="col-5">
+                    <table>
+                        <th width="100">TOTAL :</th>
+                        <th width='100'><input disabled class='col-8'  id='QttTot' text-align='center' value='<?php echo $qtot ?>'></th>
+                        <th width='100'><input disabled class='col-8' id='Prixtotal' text-align='center' value='<?php echo $Prixtot ?>'>€</th>
+                    </table>
+                </div>    
+            </div>        
+                    
+            <input disabled id='maxid' value='<?php echo $MaxId; ?>' hidden="TRUE">                                                                                                            
+<script>
+    
+    $(".click").click(function()
+    {
+       $id=$(this).attr('id');
+    });
+    
+    $(".commande").change(function()
+    {
+        //console.log($id);
+        let $prix= $("#prix"+$id).val();
+        //console.log ($prix);
+        let $quantite = $("#quantite"+$id).val();
+        $facture= $prix*$quantite;
+        //console.log($facture);
+        $("#facture"+$id).appendTo("#facture"+$id).val($facture);
+    });
+    
+    $("#tableau").click(function()
+        {
+            let $MaxId= $('#maxid').val();
+            console.log($MaxId);
+            $i=1;
+            $Prixtot = 0;
+            $Qttot=0;
+            for($i=1;$i<$MaxId;$i++)
             {
-                let $commande = $("#commande").val();
-                let $prix = $("#prix").val();
-                console.log($commande);
-                console.log($prix);
-                
-                $facture=$commande * $prix;
-                console.log($facture);
-                $("#facture").appendTo("#facture").val($facture);
-            });
-    </script>
+                $prixqtt = $("#facture"+$i).val();
+                console.log($prixqtt);
+                $Prixtot= parseInt($Prixtot) + parseInt($prixqtt);
+                console.log($Prixtot);
+                $qtt=$("#quantite"+$i).val();
+                $Qttot = parseInt($Qttot) + parseInt($qtt);
+        
+            }
+            console.log($Prixtot);
+            $("#Prixtotal").appendTo("#Prixtotal").val($Prixtot);
+            $("#QttTot").appendTo("#QttTot").val($Qttot);
+        });    
+</script>    
